b1ee4047d83290db64c8eb1483bb85fd489907ac
MafeGarciaM <mf.garciam1@uniandes.edu.co>
2022-11-20 07:06:05 -0500

    Feat: Gestos en pantalla

--- app/src/main/java/com/example/misw4203_202215_movil_grupo29/ui/MainActivity.kt
+++ app/src/main/java/com/example/misw4203_202215_movil_grupo29/ui/MainActivity.kt
@@ -1,28 +1,11 @@
 package com.example.misw4203_202215_movil_grupo29.ui
 
 import android.content.Intent
-import android.graphics.BitmapFactory
 import android.os.Bundle
 import android.util.Log
-import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.navigateUp
-import androidx.navigation.ui.setupActionBarWithNavController
-import android.view.Menu
-import android.view.MenuItem
-import android.widget.Button
-import android.widget.ImageView
 import android.widget.Toast
-import androidx.navigation.NavController
-import androidx.navigation.fragment.NavHostFragment
-import androidx.recyclerview.widget.LinearLayoutManager
-import com.example.misw4203_202215_movil_grupo29.BaseActivity
-import com.example.misw4203_202215_movil_grupo29.R
 import com.example.misw4203_202215_movil_grupo29.databinding.ActivityMainBinding
-import com.example.misw4203_202215_movil_grupo29.utilities.BitmapScaler
-import com.example.misw4203_202215_movil_grupo29.utilities.DeviceDimensionsHelper
 
 class MainActivity : AppCompatActivity() {
     private val tag  = "MainActivity"
@@ -37,7 +20,7 @@ class MainActivity : AppCompatActivity() {
         binding.initButton.setOnClickListener{
             Log.d(tag,"Start Button")
             Toast.makeText(this,"Welcome!", Toast.LENGTH_SHORT).show()
-            val intent  = Intent(this,BaseActivity::class.java)
+            val intent  = Intent(this, BaseActivity::class.java)
             startActivity((intent))
         }
 
