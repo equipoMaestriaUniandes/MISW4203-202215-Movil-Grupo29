f3c9f4ab278e0a77c77aa6335975e7d10bdb2fab
MafeGarciaM <mf.garciam1@uniandes.edu.co>
2022-11-20 05:33:27 -0500

    Feat: Pantalla de Albumes

--- app/src/main/java/com/example/misw4203_202215_movil_grupo29/BaseActivity.kt
+++ app/src/main/java/com/example/misw4203_202215_movil_grupo29/BaseActivity.kt
@@ -3,9 +3,9 @@ package com.example.misw4203_202215_movil_grupo29
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class BaseActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_base)
     }
 }
--- app/src/main/java/com/example/misw4203_202215_movil_grupo29/ui/MainActivity.kt
+++ app/src/main/java/com/example/misw4203_202215_movil_grupo29/ui/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.example.misw4203_202215_movil_grupo29.ui
 
+import android.content.Intent
+import android.graphics.BitmapFactory
 import android.os.Bundle
 import android.util.Log
 import com.google.android.material.snackbar.Snackbar
@@ -10,18 +12,37 @@ import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.Toast
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.misw4203_202215_movil_grupo29.BaseActivity
 import com.example.misw4203_202215_movil_grupo29.R
 import com.example.misw4203_202215_movil_grupo29.databinding.ActivityMainBinding
+import com.example.misw4203_202215_movil_grupo29.utilities.BitmapScaler
+import com.example.misw4203_202215_movil_grupo29.utilities.DeviceDimensionsHelper
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var navController: NavController
+    private val tag  = "MainActivity"
 
     override fun onCreate(savedInstanceState: Bundle?) {
+
+        Log.d(tag,"OnCreate")
         super.onCreate(savedInstanceState)
         val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.initButton.setOnClickListener{
+            Log.d(tag,"Start Button")
+            Toast.makeText(this,"Welcome!", Toast.LENGTH_SHORT).show()
+            val intent  = Intent(this,BaseActivity::class.java)
+            startActivity((intent))
+        }
+
+
+
 /*
         // Get the navigation host fragment from this Activity
         val navHostFragment = supportFragmentManager
